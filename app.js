ad87b5d5f89ba79462ddd31e7489e7d8c632785e
carlamt25 <57832951+carlamt25@users.noreply.github.com>
2020-09-19 19:22:00 +0200

    mongodb

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const cors = require('cors');
-
 require('dotenv').config();
-
 const dbPath = process.env.MONGODB_URI;
-
 mongoose
 	.connect(dbPath, {
 		useCreateIndex: true,
@@ -23,12 +20,8 @@ mongoose
 	.catch(error => {
 		console.error(error);
 	});
-
-const authRouter = require('./routes/auth');
-const demoRouter = require('./routes/demo');
-
 const app = express();
-
+// const offerRouter = require('./routes/offer')
 app.use(
 	cors({
 		credentials: true,
@@ -49,29 +42,23 @@ app.use(
 		secret: process.env.SECRET_SESSION,
 		resave: true,
 		saveUninitialized: true,
-		name: 'ironhack',
+		name: 'tipnoo',
 		cookie: {
 			maxAge: 24 * 60 * 60 * 1000,
 		},
 	})
 );
-
-app.use('/', authRouter);
-app.use('/protected', demoRouter);
-
+app.use('/esports-offer', offerRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
 	res.status(404).json({ code: 'not found' });
 });
-
 app.use((err, req, res, next) => {
 	// always log the error
 	console.error('ERROR', req.method, req.path, err);
-
 	// only render if the error ocurred before sending the response
 	if (!res.headersSent) {
 		res.status(500).json({ code: 'unexpected', error: err });
 	}
 });
-
 module.exports = app;
